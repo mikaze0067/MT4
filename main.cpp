cace5e81578b0d69ea8992f30c5f5ef6cbc854cf
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-10-28 15:00:40 +0900

    1-1完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
 #include <Novice.h>
+#include <cassert>
+#include <cmath>
 
 const char kWindowTitle[] = "LC1C_16_ササノ_ミカゼ";
+
+struct Vector3 {
+	float x, y, z;
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+float Dot(const Vector3& v1, const Vector3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+float Length(const Vector3& v) { return std::sqrt(Dot(v, v)); }
+
+
+Vector3 Normalize(const Vector3& v) {
+	float length = Length(v);
+	assert(length != 0.0f);
+	return { v.x / length, v.y / length, v.z / length };
+}
+
+Matrix4x4 MakeRotateAxisAngle(const Vector3& axis, float angle) {
+
+
+	//資料p20を参考に中身を埋める。nはaxisのこと
+	float cos = std::cos(angle);
+	float sin = std::sin(angle);
+
+	Matrix4x4 rotateMatrix = { 0 };
+	rotateMatrix.m[0][0] = axis.x * axis.x * (1.0f - cos) + cos;
+	rotateMatrix.m[0][1] = axis.y * axis.x * (1.0f - cos) + axis.z * sin;
+	rotateMatrix.m[0][2] = axis.z * axis.x * (1.0f - cos) - axis.y * sin;
+
+	rotateMatrix.m[1][0] = axis.x * axis.y * (1.0f - cos) - axis.z * sin;
+	rotateMatrix.m[1][1] = axis.y * axis.y * (1.0f - cos) + cos;
+	rotateMatrix.m[1][2] = axis.z * axis.y * (1.0f - cos) + axis.x * sin;
+
+	rotateMatrix.m[2][0] = axis.x * axis.z * (1.0f - cos) + axis.y * sin;
+	rotateMatrix.m[2][1] = axis.y * axis.z * (1.0f - cos) - axis.x * sin;
+	rotateMatrix.m[2][2] = axis.z * axis.z * (1.0f - cos) + cos;
+
+	rotateMatrix.m[3][3] = 1.0f;
+
+	return rotateMatrix;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.03f",
+				matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -25,6 +83,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 axis = Normalize(Vector3{ 1.0f, 1.0f, 1.0f });
+		float angle = 0.44f;
+		Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +94,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, rotateMatrix, "rotateMatrix");
 
 		///
 		/// ↑描画処理ここまで
