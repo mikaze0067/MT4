8e08716a2087ad83c86c6e5e84c3c73de01ee4e9
mikaze <146914469+mikaze0067@users.noreply.github.com>
2025-01-29 12:24:01 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 #include <cmath>
 
-const char kWindowTitle[] = "LC1C_16_ササノ_ミカゼ";
+const char kWindowTitle[] = "Mt4_LC1D_09_ササノ_ミカゼ";
 
 struct Vector3 {
 	float x, y, z;
@@ -15,6 +15,10 @@ struct Matrix4x4 {
 float Dot(const Vector3& v1, const Vector3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
 float Length(const Vector3& v) { return std::sqrt(Dot(v, v)); }
 
+Vector3 Cross(const Vector3& v1, const Vector3& v2) {
+	return { v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
+}
+
 
 Vector3 Normalize(const Vector3& v) {
 	float length = Length(v);
@@ -22,29 +26,71 @@ Vector3 Normalize(const Vector3& v) {
 	return { v.x / length, v.y / length, v.z / length };
 }
 
-Matrix4x4 MakeRotateAxisAngle(const Vector3& axis, float angle) {
 
 
-	//資料p20を参考に中身を埋める。nはaxisのこと
-	float cos = std::cos(angle);
-	float sin = std::sin(angle);
+Matrix4x4 DirectionToDirection(const Vector3& from, const Vector3& to) {
+	// 正規化
+	Vector3 u = Normalize(from);
+	Vector3 v = Normalize(to);
 
-	Matrix4x4 rotateMatrix = { 0 };
-	rotateMatrix.m[0][0] = axis.x * axis.x * (1.0f - cos) + cos;
-	rotateMatrix.m[0][1] = axis.y * axis.x * (1.0f - cos) + axis.z * sin;
-	rotateMatrix.m[0][2] = axis.z * axis.x * (1.0f - cos) - axis.y * sin;
+	//from,toのクロス積を取る
+	Vector3 cross = Cross(u, v);
 
-	rotateMatrix.m[1][0] = axis.x * axis.y * (1.0f - cos) - axis.z * sin;
-	rotateMatrix.m[1][1] = axis.y * axis.y * (1.0f - cos) + cos;
-	rotateMatrix.m[1][2] = axis.z * axis.y * (1.0f - cos) + axis.x * sin;
+		//from,toの内積を取る
+		float cos = Dot(u, v);
 
-	rotateMatrix.m[2][0] = axis.x * axis.z * (1.0f - cos) + axis.y * sin;
-	rotateMatrix.m[2][1] = axis.y * axis.z * (1.0f - cos) - axis.x * sin;
-	rotateMatrix.m[2][2] = axis.z * axis.z * (1.0f - cos) + cos;
+		//from,toの長さを取る
+		float sin = Length(cross);
 
+		float epsilon = 1e-6f;
+	Vector3 axis = {};
+	if (std::abs(cos + 1.0f) <= epsilon) {
+		if (std::abs(from.x) > epsilon || std::abs(from.y) > epsilon) {
+			//(ux≠0||uy≠0)の際のaxisの値を入れる
+			axis.x = -from.y;
+			axis.y = from.x;
+			axis.z = 0.0f;
+		}
+		else if (std::abs(from.x) > epsilon || std::abs(from.z) > epsilon) {
+			//(ux≠0||uz≠0)の際のaxisの値を入れる
+			axis.x = -from.z;
+			axis.y = 0.0f;
+			axis.z = from.x;
+		}
+		else {
+			// zero vector
+			assert(false);
+		}
+
+		axis = Normalize(axis);
+	}
+	else {
+		axis = Normalize(cross);
+	}
+
+	//確認課題01_01同様中身を埋める
+	Matrix4x4 rotateMatrix = {};
+	rotateMatrix.m[0][0] = (1.0f - cos) * axis.x * axis.x + cos;
+	rotateMatrix.m[0][1] = (1.0f - cos) * axis.x * axis.y + sin * axis.z;
+	rotateMatrix.m[0][2] = (1.0f - cos) * axis.x * axis.z - sin * axis.y;
+	rotateMatrix.m[0][3] = 0.0f;
+
+	rotateMatrix.m[1][0] = (1.0f - cos) * axis.y * axis.x - sin * axis.z;
+	rotateMatrix.m[1][1] = (1.0f - cos) * axis.y * axis.y + cos;
+	rotateMatrix.m[1][2] = (1.0f - cos) * axis.y * axis.z + sin * axis.x;
+	rotateMatrix.m[1][3] = 0.0f;
+
+	rotateMatrix.m[2][0] = (1.0f - cos) * axis.z * axis.x + sin * axis.y;
+	rotateMatrix.m[2][1] = (1.0f - cos) * axis.z * axis.y - sin * axis.x;
+	rotateMatrix.m[2][2] = (1.0f - cos) * axis.z * axis.z + cos;
+	rotateMatrix.m[2][3] = 0.0f;
+
+	rotateMatrix.m[3][0] = 0.0f;
+	rotateMatrix.m[3][1] = 0.0f;
+	rotateMatrix.m[3][2] = 0.0f;
 	rotateMatrix.m[3][3] = 1.0f;
 
-	return rotateMatrix;
+		return rotateMatrix;
 }
 
 static const int kRowHeight = 20;
@@ -64,11 +110,17 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	Vector3 v1{ 1.0f, 3.0f, -5.0f };
+	Vector3 v2{ 4.0f, -1.0f, 2.0f };
+	// float k = { 4.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -83,9 +135,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 axis = Normalize(Vector3{ 1.0f, 1.0f, 1.0f });
-		float angle = 0.44f;
-		Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
+		Vector3 from0 = Normalize(Vector3{ 1.0f, 0.7f, 0.5f });
+		Vector3 to0 = { -from0.x,-from0.y, -from0.z };
+		Vector3 from1 = Normalize(Vector3{ -0.6f, 0.9f, 0.2f });
+		Vector3 to1 = Normalize(Vector3{ 0.4f, 0.7f, -0.5f });
+		Matrix4x4 rotateMatrix0 = DirectionToDirection(
+			Normalize(Vector3{ 1.0f, 0.0f, 0.0f }), Normalize(Vector3{ -1.0f, 0.0f, 0.0f }));
+		Matrix4x4 rotateMatrix1 = DirectionToDirection(from0, to0);
+		Matrix4x4 rotateMatrix2 = DirectionToDirection(from1, to1);
 
 		///
 		/// ↑更新処理ここまで
@@ -95,7 +152,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0, 0, rotateMatrix, "rotateMatrix");
+		MatrixScreenPrintf(0, 0, rotateMatrix0, "rotateMatrix0");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateMatrix1, "rotateMatrix1");
+		MatrixScreenPrintf(0, kRowHeight * 10, rotateMatrix2, "rotateMatrix2");
 
 		///
 		/// ↑描画処理ここまで
